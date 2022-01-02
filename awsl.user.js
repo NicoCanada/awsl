cdb08886c8be02a9c64123c50367502f3b82d763
Nico <38353910+NicoCanada@users.noreply.github.com>
2022-01-01 23:56:30 -0600

    Update awsl.user.js

--- awsl.user.js
+++ awsl.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AWSL
 // @namespace    https://github.com/xingrz
-// @version      0.4.1
+// @version      0.4.2
 // @description  Auto AWSLing
 // @author       XiNGRZ <hi@xingrz.me>
 // @license      WTFPL
