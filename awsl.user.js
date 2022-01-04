9395564b3d401b1ddedabdd4bea923447b7f09e9
Nico <38353910+NicoCanada@users.noreply.github.com>
2022-01-03 23:47:57 -0600

    Update awsl.user.js
    to prevent auto update from https://github.com/xingrz

--- awsl.user.js
+++ awsl.user.js
@@ -1,17 +1,17 @@
 // ==UserScript==
 // @name         AWSL
 // @namespace    https://github.com/xingrz
-// @version      0.4.2
+// @version      0.4.3
 // @description  Auto AWSLing
 // @author       XiNGRZ <hi@xingrz.me>
 // @license      WTFPL
 // @match        https://*weibo.com/*
 // @grant        GM.getValue
 // @grant        GM.setValue
-// @updateURL    https://raw.githubusercontent.com/NicoCanada/awsl/master/awsl.meta.js
-// @downloadURL  https://raw.githubusercontent.com/NicoCanada/awsl/master/awsl.user.js
-// @homepageURL  https://github.com/xingrz/awsl
-// @supportURL   https://github.com/xingrz/awsl/issues
+// @updateURL    https://github.com/NicoCanada/awsl/raw/master/awsl.user.js
+// @downloadURL  https://github.com/NicoCanada/awsl/raw/master/awsl.user.js
+// @homepageURL  https://github.com/NicoCanada/awsl
+// @supportURL   https://github.com/NicoCanada/awsl/issues
 // ==/UserScript==
 
 'use strict';
